de32cbdf46c3a5a1bc38bfcaf89402ea68ede5ee
Elina Kadyrova <kad_elina@mail.ru>
2021-07-05 21:28:50 +0300

    settings pages done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_login_light)
+        setContentView(R.layout.fragment_settings_dark)
     }
 }
