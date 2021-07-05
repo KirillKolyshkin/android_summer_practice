8aac02209a28dfb40abdb03e3c5de27c9f0eea72
Elina Kadyrova <kad_elina@mail.ru>
2021-07-06 02:06:47 +0300

    main fragments done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.item_task_lt)
+        setContentView(R.layout.fragment_login_light)
     }
 }
