d5635d8fefa0053152c2ffdfe93028d95f7e036a
AnastasiaChukhina <nas2002@list.ru>
2021-07-06 16:42:40 +0300

    tasks table created

---
+++ app/src/main/java/com/example/myapplication/data/entities/Task.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val task_id: Int,
+    @ColumnInfo(name = "text")
+    var text: String,
+    @ColumnInfo(name = "timestamp")
+    var timestamp: Long,
+    @ColumnInfo(name = "state")
+    var isDode: Boolean,
+)
