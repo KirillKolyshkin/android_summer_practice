8c8e489c0a3376e02ab3340ceb90ed1b8c77823f
AnastasiaChukhina <nas2002@list.ru>
2021-07-07 11:25:46 +0300

    TaskRepository and TaskViewModel added

--- app/src/main/java/com/example/myapplication/data/AppDatabase.kt
+++ app/src/main/java/com/example/myapplication/data/AppDatabase.kt
@@ -4,12 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.myapplication.data.dao.TaskDao
 import com.example.myapplication.data.entities.Task
 
 @Database(entities = [Task::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao() : TaskDao
-
 
     companion object{
         @Volatile
