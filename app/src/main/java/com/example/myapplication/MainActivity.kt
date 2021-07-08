3d67d3fbd910123f07a17e8fb9b6311e5643788d
Elina Kadyrova <kad_elina@mail.ru>
2021-07-08 16:13:04 +0300

    themes added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ import com.example.myapplication.data.UserDatabase
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_main_light)
+        setContentView(R.layout.fragment_main)
 
         val userDB = UserDatabase.getDatabase(this).userDao()
     }
