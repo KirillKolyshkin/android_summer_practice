3b332516c410f670ec4b4cbba3f3919013c1c6a0
Adelya <valisheva2002@mail.ruclear>
2021-07-09 15:31:11 +0300

    1 conflict fixed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,18 +3,18 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.data.UserDatabase
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //startActivityFromFragment(R.layout.fragment_login_light)
-
-
         setContentView(R.layout.fragment_login_light)
         setContentView(R.layout.fragment_main)
 
         val userDB = UserDatabase.getDatabase(this).userDao()
-
 
 
     }
--- app/src/main/java/com/example/myapplication/fragment/LoginLightFragment.kt
+++ app/src/main/java/com/example/myapplication/fragment/LoginLightFragment.kt
@@ -1,20 +1,27 @@
 package com.example.myapplication.fragment
 
 
+<<<<<<< HEAD
 
 import androidx.fragment.app.Fragment
+=======
+>>>>>>> 5185b69... the loginPage a little done
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+<<<<<<< HEAD
 import android.widget.Toast
+=======
+>>>>>>> 5185b69... the loginPage a little done
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.myapplication.R
 import com.example.myapplication.data.entities.User
 import com.example.myapplication.data.view_model.UserViewModel
+<<<<<<< HEAD
 import com.example.myapplication.fragment.main.MainFragment
 import kotlin.random.Random
 
@@ -38,12 +45,22 @@ class LoginLightFragment : Fragment(R.layout.fragment_login_light)  {
         "©Jean de La Bruyère",
         "©Anthony Doerr",
     )
+=======
+
+
+class LoginLightFragment() : Fragment(){
+
+    private lateinit var userViewModel : UserViewModel
+
+
+>>>>>>> 5185b69... the loginPage a little done
 
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+<<<<<<< HEAD
 
     ): View {
         val view: View =  inflater.inflate(R.layout.fragment_login_light, container, false)
@@ -54,6 +71,20 @@ class LoginLightFragment : Fragment(R.layout.fragment_login_light)  {
 
         val authorEt : EditText = view.findViewById<EditText>(R.id.quote_author)
         val quoteEt : EditText = view.findViewById<EditText>(R.id.quote)
+=======
+    ): View {
+        val view: View =  inflater.inflate(R.layout.fragment_login_light, container, false)
+
+        val loginBtn: Button = view.findViewById(R.id.button)
+
+        //userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        loginBtn.setOnClickListener(){
+
+            insertDataToDatabase()
+            view.findNavController().navigate(R.id.action_login_light_to_main_light)
+       }
+>>>>>>> 5185b69... the loginPage a little done
 
         authorEt.setText(quoteAuthor)
         quoteEt.setText(quote)
@@ -62,8 +93,28 @@ class LoginLightFragment : Fragment(R.layout.fragment_login_light)  {
 
         val loginBtn: Button = view.findViewById(R.id.button)
 
+<<<<<<< HEAD
         val name : String = view.findViewById<EditText>(R.id.et_username).toString()
 
+=======
+    private fun insertDataToDatabase() {
+        val name : String = view?.findViewById<EditText>(R.id.et_username).toString()
+
+//        if(name.isNotEmpty()){
+//            val user = User(name)
+//            userViewModel.addUser(user)
+//        }
+    }
+
+
+
+
+
+
+
+
+}
+>>>>>>> 5185b69... the loginPage a little done
 
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
